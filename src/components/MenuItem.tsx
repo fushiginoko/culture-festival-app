22e5b0edfa73f7c9637e3695cf9e6161aac4b96c
fushiginoko <fushiginoko@gmail.com>
2026-08-17 06:44:47 +0900

    Refactor MenuItem to accept props

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,13 +1,9 @@
-function MenuItem() {
-  // 仮の値
-  const id: number = 1
-  const name: string = "焼きそば"
-  const price: number = 300
+function MenuItem(props) {
 
   return (
     <div>
-      <h2>{name}</h2>
-      <p>{price}円</p>
+      <h2>{props.name}</h2>
+      <p>{props.price}円</p>
     </div>
   )
 }
