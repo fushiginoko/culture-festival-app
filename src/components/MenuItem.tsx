6f5876e0027872ae25640744fe86caa269de57ae
fushiginoko <fushiginoko@gmail.com>
2026-08-17 06:23:37 +0900

    Render menu item details in MenuItem component

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -3,4 +3,11 @@ function MenuItem() {
   const id: number = 1
   const name: string = "焼きそば"
   const price: number = 300
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{price}円</p>
+    </div>
+  )
 }
