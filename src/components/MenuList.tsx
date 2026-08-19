d51ff026b4d392de1a0b433694396cd7d2120517
fushiginoko <fushiginoko@gmail.com>
2026-08-19 16:29:28 +0900

    Refactor MenuList to accept items as props

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -1,15 +1,15 @@
 import MenuItem from "./MenuItem"
 
-function MenuList() {
-  // 仮の値
-  const menuItems = [
-    {id: 1, name: "ハンバーガー", price: 300 },
-    {id: 2, name: "フライドチキン", price: 400 },
-    {id: 3, name: "ピザ", price: 500 },
-  ]
+interface MenuItem {
+  id: number;
+  name: string;
+  price: number;
+}
+
+function MenuList(props: { items: MenuItem[] }) {
   return (
     <div>
-      {menuItems.map((item) => (
+      {props.items.map((item) => (
         <MenuItem
           key={item.id}
           name={item.name}
--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -1,9 +1,17 @@
 import MenuList from "../components/MenuList";
+import type { MenuItem } from "../components/MenuList";
 
 function MenuPage() {
+  // 仮の値
+  const menuItems: MenuItem[] = [
+    {id: 1, name: "ハンバーガー", price: 300 },
+    {id: 2, name: "フライドチキン", price: 400 },
+    {id: 3, name: "ピザ", price: 500 },
+  ]
+
   return (
     <div>
-      <MenuList />
+      <MenuList items={menuItems} />
     </div>
   )
 }
