a45a5717e00aacd02d1556e633dfa97b5f2ffdf9
fushiginoko <fushiginoko@gmail.com>
2026-09-20 05:50:33 +0900

    Extract selected items from router state in PickupTimePage

--- src/pages/PickupTimePage.tsx
+++ src/pages/PickupTimePage.tsx
@@ -1,4 +1,17 @@
+import { useLocation, Navigate } from "react-router-dom";
+
+type OrderItem = {
+  id: number;
+  name: string;
+  price: number;
+  count: number;
+};
+
 function PickupTimePage() {
+  const location = useLocation();
+  const state = location.state as { selectedItems: OrderItem[] } | undefined;
+  const selectedItems = state?.selectedItems ?? [];
+
   return (
     <>
 
